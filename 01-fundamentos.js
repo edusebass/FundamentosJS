53c3d2d37a13c5049235614cee38ce830a8858f9
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2023-11-13 15:13:29 -0500

    mas ejemplos de metodos de strings

--- 01-fundamentos.js
+++ 01-fundamentos.js
@@ -41,3 +41,27 @@ console.log("Cortar un texto:",nombreJuego.substring(1,6));
 console.log("Cortar un texto:",nombreJuego.charAt(1));
 console.log("Repetir un texto:",nombreJuego.repeat(2));
 console.log("Dividir un texto:",nombreJuego.split(" "));
+
+'use strict';
+
+let formUsername = 'Tosh'
+console.log("Útil para validar el campo nombre del formulario:",formUsername.length);
+let emailUser = 'byron@epn.edu.ec'
+console.log("Útil para encotrar algún patrón:",emailUser.includes("@epn.edu.ec"));
+let rolUser = 'Administrador'
+console.log("Útil para convertir a mayúsculas y presentar en la UI:",rolUser.toUpperCase());
+let addressUser = 'QUITUMBE ÑAN 456'
+console.log("Útil para convertir a minusculas y guardar en BDD:",addressUser.toLowerCase());
+let formLastName = '    Loarte  '
+console.log("Útil para quitar espacios adelante y atrás en el campo del formulario:",formLastName.trim());
+let discountProduct = "El precio de descuento es del 20%"
+console.log("Útil para reemplazar el precio de un descuento:",discountProduct.replace('20%','50%'));
+let detailProduct = "Mouse gamer de juego inalámbrico Logitech Serie G Lightspeed G305 blue - Logitech diseña productos y experiencias que ocupan un lugar cotidiano en la vida de las personas, poniendo foco en la innovación y la calidad. "
+console.log("Útil para hacer el efecto leer más o expander de un texto largo:",detailProduct.slice(0,50)+'....');
+let creditCardUser = "1234 5678 9851 9937"
+console.log("Útil para cortar número de tarjeta de crédito en el proceso de facturación:",creditCardUser.substring(15,19));
+let numFlight = "AA4569"
+console.log("Útil para saber el prefijo del vuelo que indica la aerolínea:",numFlight.charAt(0)+numFlight.charAt(1));
+let userAdmin = "Byron Loarte"
+let avatarUser = userAdmin.split(" ")
+console.log("Útil para cortar un texto y colocar como avatar de perfil:",avatarUser[0].charAt(0),avatarUser[1].charAt(0));
