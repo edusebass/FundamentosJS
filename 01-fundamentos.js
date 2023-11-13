cdad9de3db7a9774813904cefcf756622f7fd652
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2023-11-13 15:05:14 -0500

    datos

--- 01-fundamentos.js
+++ 01-fundamentos.js
@@ -3,6 +3,26 @@ console.log("holallalalalala")
 console.log("Que mas oyen")
 console.log("Que mas oyen")
 
+
 //Comentario de una linea
 /*Comentarios de varias
 lineas*/ 
+
+
+//variables
+/* var no se recomienda*/
+let nombre = "Juan"
+let puntosVida = 234
+
+dato = 12.4
+dato = "HOOLA"
+dato = '@'
+dato = false
+dato = 12.4
+dato = [12, true, 89]
+dato = {
+    nombre: "Juan",
+    puntosVida: 234
+}
+
+console.log(dato.puntosVida)
