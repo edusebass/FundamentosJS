4c03f56a68063b8ea139d1c4966a48976c407c07
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2023-11-13 15:11:24 -0500

    strings metodos

--- 01-fundamentos.js
+++ 01-fundamentos.js
@@ -26,3 +26,18 @@ dato = {
 }
 
 console.log(dato.puntosVida)
+
+//strings
+let nombreJuego = "MarioKart"
+
+console.log("Número de letras:",nombreJuego.length);
+console.log("Encotrar alguna palabra:",nombreJuego.includes("Kart"));
+console.log("Convertir a mayúsculas:",nombreJuego.toUpperCase());
+console.log("Convertir a minusculas:",nombreJuego.toLowerCase());
+console.log("Quitar espacios adelante y atrás:",nombreJuego.trim());
+console.log("Reemplazar una palabra:",nombreJuego.replace('Kart','Kart-Rider'));
+console.log("Cortar un texto:",nombreJuego.slice(1,6));
+console.log("Cortar un texto:",nombreJuego.substring(1,6));
+console.log("Cortar un texto:",nombreJuego.charAt(1));
+console.log("Repetir un texto:",nombreJuego.repeat(2));
+console.log("Dividir un texto:",nombreJuego.split(" "));
