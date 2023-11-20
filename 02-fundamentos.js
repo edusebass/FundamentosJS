119045d519552cdcf476723efd1ef2e8a429ab37
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2023-11-20 14:28:55 -0500

    reacomodo codigo

--- 02-fundamentos.js
+++ 02-fundamentos.js
@@ -1,9 +1,10 @@
-//OBJETO
+// OBJETOS
+
 const user = {
-    name:"Byron",
-    lastname:"Loarte",
-    age:34,
-    email:"eduardo@gmail.com",
+    name: "Byron",
+    lastname: "Loarte",
+    age: 34,
+    email: "eduardo@gmail.com",
     activo: true,
     address: {
         city: "Madrid",
@@ -11,68 +12,59 @@ const user = {
         calle: "Calle abc",
         secundaria: "calle 1123312"
     },
-    edad:35,
+    edad: 35,
     bestFriend: {
         name: "Peter",
         lastname: "Loarte",
-        age:34
+        age: 34
     },
-    moreFriends: ["Peter", "JOse", "Mijin"],
-    sendMail(){
-        return `send mail to ${this.moreFriends[1]}`
+    moreFriends: ["Peter", "Jose", "Mijin"],
+    sendMail() {
+        return `send mail to ${this.moreFriends[1]}`;
     }
-}
+};
 
-console.log(JSON.stringify(user))
-console.log((user))
+console.log(JSON.stringify(user));
+console.log(user);
 
-//desustrucutaracion de objetos
-const {name, edad , moreFriends, activo} = user
-console.log(user)
+// Desestructuración de Objetos
+const { name, edad, moreFriends, activo } = user;
+console.log(name, edad, moreFriends, activo);
 
-//unir objetos
+// Unir Objetos
 const address = {
-    city: "granada",
-    country: "croacia",
+    city: "Granada",
+    country: "Croacia",
     calle: "Calle 12323",
     secundaria: "calle secun"
-}
+};
 
-const allinformation = {...user, ...address}
-console.log(allinformation)
+const allInformation = { ...user, ...address };
+console.log(allInformation);
 
+// ARREGLOS
 
-//ARREGLOS
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-const arr = [1,2,3,4,5,6,7,8,9,10]
+console.log(arr.length);
+console.log(arr[0]);
+console.log(arr[9]);
 
-console.log(arr.length)
-console.log(arr[0])
-console.log(arr[9])
+// Métodos de Modificación de Arreglos
+arr.push(11);   // Agrega 11 al final del arreglo.
+arr.unshift(0);  // Agrega 0 al inicio del arreglo.
+arr.pop();       // Elimina el último elemento del arreglo.
+arr.shift();     // Elimina el primer elemento del arreglo.
 
-// arr.push() mete al final
-// arr.unshift(1) mete al incio
+console.log("Modified Array:", arr);
 
-// arr.pop() elimina el ultimo
-// arr.shift() elimina el ultimo
+// Método find y map en Arreglos
+const findOne = arr.find(e => e !== 9);
+console.log("Find One:", findOne); // Encuentra el primer elemento que no es 9.
 
+const newData = arr.map(e => e * 2);
+console.log("New Data (doubled):", newData);
 
-const findOne =  arr.find( e => e != 9)
-const findTwo =  arr.find((e) => e === "Juan")
-
-console.log("Console principal: ", arr)
-
-console.log(findOne)
-console.log(findTwo)
-
-const arreglo = ["1", "2", "3", "4", "eduardo"]
-
-const newData = arreglo.map((e) => { 
-    return e.toUpperCase()
-})
-console.log(newData)
-
-
-//desestructuracion
-const [numero1, numero2, numero3, numero4, names] = arreglo
-console.log(numero1, numero2, numero3, numero4, names)
+// Desestructuración de Arreglos
+const [numero1, numero2, numero3, numero4, names] = arr;
+console.log("Destructured Array:", numero1, numero2, numero3, numero4, names);
